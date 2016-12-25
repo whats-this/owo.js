da74cc7d09a042d686fbc731e186d43709ce6002
Zaidhaan Hussain <zaidhanhussain@gmail.com>
2016-12-25 14:37:14 +0500

    fix indents

--- index.js
+++ index.js
@@ -11,79 +11,72 @@ const request = require("request");
 key = "";
 
 exports.setKey = function(userkey) {
-	key = userkey;
+  key = userkey;
 }
 
 exports.uploadFile = function(file, userkey) {
-	return new Promise((resolve, reject) => {
-		if(key != "") {
-			userkey = key
-		} else {
-			if(userkey == undefined){
-				reject('OwO : ERROR : Userkey undefined.');
-				return;
-			}
-		}
+  return new Promise((resolve, reject) => {
+    if(key != "") {
+	  userkey = key
+	} else {
+	  if(userkey == undefined){
+		reject('OwO : ERROR : Userkey undefined.');
+		return;
+	  }
+	}
 
-		let options = { method: 'POST',
-			url: 'https://api.awau.moe/upload/pomf',
-			headers: {
-				authorization: userkey
-			},
-			formData: { 'files[]':
-				{
-					value: fs.createReadStream(file),
-					options: {
-						filename: file, contentType: null
-					}
-				}
-			}
-		};
+	let options = {
+	  method: 'POST',
+	  url: 'https://api.awau.moe/upload/pomf',
+	  headers: {
+	    authorization: userkey
+	  },
+	  formData: { 'files[]': {
+		value: fs.createReadStream(file),
+		options: {
+		  filename: file, contentType: null
+		}
+	  }}
+    };
 
 		/** BEGIN REQUEST**/
-		request(options, function (error, response, body) {
-			if(error) reject(error)
-			resolve(body);
-			response.on('data', function (chunk) {
-				body += chunk;
-			});
-			response.on('end', function () {
-				/*console.log("BODY LATER"+body);*/
-			});
-		});
+	request(options, function (error, response, body) {
+	  if(error) reject(error)
+	  resolve(body);
 	});
+  });
 };
 
 exports.shortenURL = function(url, userkey){
-	return new Promise((resolve, reject) => {
-		if(key != "") {
-			userkey = key
-		} else {
-			if(userkey == undefined){
-				reject('OwO : ERROR : Userkey undefined.');
-				return;
-			}
-		}
+  return new Promise((resolve, reject) => {
+    if(key != "") {
+	  userkey = key
+	} else {
+	  if(userkey == undefined){
+		reject('OwO : ERROR : Userkey undefined.');
+		return;
+	  }
+	}
 
-		if(url == undefined || url == ""){
-			reject('OwO : ERROR : URL is not specified.');
-			return;
-		}
+	if(url == undefined || url == ""){
+	  reject('OwO : ERROR : URL is not specified.');
+	  return;
+	}
 
-		let options = {
-			url: "https://api.awau.moe/shorten/polr",
-			headers: {
-				authorization: userkey
-			},
-			qs: {
-				action: "shorten",
-				url: url
-			}
-		}
+	let options = {
+	  url: "https://api.awau.moe/shorten/polr",
+	  headers: {
+	    authorization: userkey
+	  },
+	  qs: {
+		action: "shorten",
+		url: url
+	  }
+	}
 
-		request(options, (error, response, body) => {
-			if(error) reject(error);
-			resolve(body);
-		});
+	request(options, (error, response, body) => {
+	  if(error) reject(error);
+	    resolve(body);
 	});
+  });
 }
