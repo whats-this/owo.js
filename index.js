377da653260f09ef08f12763605ee3b916f2675f
Jakeoid <me@jakeoid.com>
2016-12-25 17:43:22 +1100

    Initial Commit
    Initial code thanks to some help by @TheVexatious and @sr229

---
+++ index.js
@@ -0,0 +1,54 @@
+//                    _     
+//  ___ _ _ _ ___    |_|___ 
+// | . | | | | . |_  | |_ -|
+// |___|_____|___|_|_| |___|
+//                 |___|    
+// 
+
+key = "";
+
+exports.setKey = function(userkey) {
+	key = userkey;
+}
+
+exports.uploadFile = function(file, userkey) {
+	return new Promise((resolve, reject) => {
+		if(key != "") {
+			userkey = key
+		} else {
+			if(userkey == undefined){
+				reject('OwO : ERROR : Userkey undefined.');
+				return;
+			}
+		}
+
+		var fs = require("fs");
+		var request = require("request");
+
+		var options = { method: 'POST',
+			url: 'https://api.awau.moe/upload/pomf',
+			headers: {
+				authorization: userkey
+			},
+			formData: { 'files[]': 
+				{
+					value: fs.createReadStream(file),
+					options: {
+						filename: file, contentType: null
+					}
+				} 
+			}
+		};
+
+		/** BEGIN REQUEST**/
+		request(options, function (error, response, body) {
+			resolve(body);
+			response.on('data', function (chunk) {
+				body += chunk;
+			}); 
+			response.on('end', function () {
+				/*console.log("BODY LATER"+body);*/
+			});
+		});
+	});	
+};
