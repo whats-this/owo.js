d62e1e4b55530639db95f6fc29285afb920efd98
Capuccino <aguilarralph13@gmail.com>
2017-01-04 10:43:20 +0800

    added sortenTest and made upload follow ES6

---
+++ spec/shortenTest.js
@@ -0,0 +1,11 @@
+// * Shorten URL tester
+// * 
+// * tests the URL Shorten function.
+
+const owo = new (require(require('path').resolve('../src/OwO.js')));
+
+describe('Shorten_test', () => {
+    it('upload', () => {
+        //Code coming soon(TM)
+    })
+});
