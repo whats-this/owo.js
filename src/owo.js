b480bd2c690cf94b43b82985944c3988e273e8b0
sr229 <aguilarralph13@gmail.com>
2017-02-25 21:28:53 +0800

    Documentation

--- src/owo.js
+++ src/owo.js
@@ -4,12 +4,19 @@ const Endpoints = require('./Endpoints');
 const request = require('superagent');
 const OwOError = require('./owoError');
 
+/*
+ * Creates a new class
+ * @prop {key} key your whats-th.is API Key.
+ */
 class OwOClient {
   constructor (key) {
     this.key = key;
   }
-
-  upload (path) {
+/*
+ * Uploads to OWO
+ * @prop {String} path the file directory name.
+ */
+    upload (path) {
     let files = (path instanceof Array) ? path : new Array(path);
     for (let index in files) files[index] = createReadStream(files[index]);
     return request.post(Endpoints.upload(this.key))
@@ -17,8 +24,11 @@ class OwOClient {
       .then((res) => res.body)
       .catch((err) => new OwOError(err.message, err.response.req, err.response));
   }
-
-  shorten (url) {
+/*
+ * shortens your desired URL.
+ * @prop {String} url your URL you desire to shorten.
+ */
+    shorten (url) {
     return request.get(Endpoints.shorten(this.key, url))
       .then((res) => res.text)
       .catch((err) => new OwOError(err.message, err.response.req, err.response));
